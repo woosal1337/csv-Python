90df620768bc578b39887bc1d1837f173f08c37e
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-17 02:05:36 +0300

    1.0

---
+++ demo.py
@@ -0,0 +1,80 @@
+import csv
+
+filename = "mails.csv"
+newUser = [6118038532665114624,"woosal@protonmail.com"]
+
+
+class implementCSV:
+    def __init__(self, filename):
+        self.filename = filename
+
+
+    # Function that takes the .csv formatted list input
+    # as an argument, and appends it as a new row to the .csv file
+    def appendCSV(self, filename, newUser):
+        with open(filename, 'a+', newline='') as writecsv:
+            csvwriter = csv.writer(writecsv)
+            csvwriter.writerow(newUser)
+
+    def readCSV(self, filename):
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+
+            next(csvreader)
+
+            for line in csvreader:
+                print(line)
+
+    # Checks whether the given user ID exists in the .csv
+    # and returns True/False boolean value
+    def checkNewUserId(self, filename, newUser):
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+
+            next(csvreader)
+
+            for line in csvreader:
+                if str(newUser[0]) == line[0]:
+                    return True
+
+        return False
+
+    # Checks whether the given user mail exists in the .csv
+    # and returns True/False boolean value
+    def checkNewUserMail(self, filename, newUser):
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+
+            next(csvreader)
+
+            for line in csvreader:
+                if str(newUser[1]) == line[1]:
+                    return True
+
+        return False
+
+    def getIdByMail(self, filename, mail):
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+
+            next(csvreader)
+
+            for line in csvreader:
+                if line[1] == mail:
+                    return str(line[0])
+
+    def getMailById(self, filename, id):
+        with open(filename, "r") as csvfile:
+            csvreader = csv.reader(csvfile)
+
+            next(csvreader)
+
+            for line in csvreader:
+                if line[0] == id:
+                    return str(line[1])
+
+
+
+csvObj = implementCSV(filename)
+
+print(csvObj.getMailById(filename, "859235792352957922"))
